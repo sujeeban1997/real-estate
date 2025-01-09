46c19552db76997f14cc23a40248e707b4e9f455
Sujeeban Gunathasan <sujeethasan0@gmail.com>
2025-01-09 16:58:39 +0530

    spacing issue22

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -20,20 +20,20 @@ function Cart() {
                     </div>
                 
                     {items.map((item) => (
-                              <div className='card'  key={item.id}>
-                                        <div className='row'>
-                                                  <div className='col-lg-4'>
-                                                            <img className="fav-image-view" src={item.imgSrc} alt='' />
-                                                  </div>
-                                                  <div className='col-lg-8'>
-                                                      <div className='fav-details'>
-                                                          <h4>{item.name}</h4>
-                                                          <h6>{item.bedrooms}</h6>
-                                                          <h6>$ {item.price}</h6>
-                                                          <button className='btn btn-outline-danger px-4 py-2' onClick={() => removeItem(item.id)}>Delete Property</button>
-                                                        </div>
-                                                  </div>
-                                        </div>
+                        <div className='card'  key={item.id}>
+                            <div className='row'>
+                                  <div className='col-lg-4'>
+                                      <img className="fav-image-view" src={item.imgSrc} alt='' />
+                                  </div>
+                                  <div className='col-lg-8'>
+                                      <div className='fav-details'>
+                                        <h4>{item.name}</h4>
+                                        <h6>{item.bedrooms}</h6>
+                                        <h6>$ {item.price}</h6>
+                                        <button className='btn btn-outline-danger px-4 py-2' onClick={() => removeItem(item.id)}>Delete Property</button>
+                                      </div>
+                                  </div>
+                                </div>
                               </div>  
                      ))}                       
           </div>
